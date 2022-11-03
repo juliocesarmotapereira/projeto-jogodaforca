5b398a9d90f0ef0d1b1c8586cf8702ec244d4e0c
julio mota <juliocesarmotapereira@gmail.com>
2022-11-02 23:51:10 -0300

    comecando codigo js e adicinando pasta imagem

---
+++ forca.js
@@ -0,0 +1,13 @@
+//Seletores
+let palavras = ["SOL", "LUA", "ESTRELA", "TERRA", "VENTO", "MAR"];
+let tabuleiro = document.getElementById("forca").getContext('2d');
+let palavraSecreta = "";
+
+function escolherPalavraSecreta(){
+    
+}
+
+
+function iniciarJogo() {
+    document.getElementById('div-desaparece').style.display = "none"
+}
